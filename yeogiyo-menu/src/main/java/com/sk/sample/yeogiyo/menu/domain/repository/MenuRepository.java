b8e3b7828a0482a9134b3558d8478e2311f2780a
djkim81 <2pac81@nate.com>
2019-08-29 10:51:35 +0900

    yeogiyo-menu , yeogiyo-shared 프로젝트 init

---
+++ yeogiyo-menu/src/main/java/com/sk/sample/yeogiyo/menu/domain/repository/MenuRepository.java
@@ -0,0 +1,24 @@
+package com.sk.sample.yeogiyo.menu.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.querydsl.core.types.Predicate;
+import com.sk.sample.yeogiyo.menu.domain.model.FoodType;
+import com.sk.sample.yeogiyo.menu.domain.model.Menu;;
+
+@RepositoryRestResource
+public interface MenuRepository extends PagingAndSortingRepository<Menu, Long>,
+                                           QueryDslPredicateExecutor<Menu> {
+	List<Menu> findAll(Predicate predicate); 
+	
+	Menu findByName(@Param("name") String name);
+	List<Menu> findByMenuDescriptionFoodType(@Param("foodType") FoodType foodType);
+	List<Menu> findByPriceValueGreaterThanEqual(@Param("value") Integer value);
+	List<Menu> findByPriceValueLessThanEqual(@Param("value") Integer value);
+	List<Menu> findByPriceValueGreaterThanAndPriceValueLessThan(@Param("value1") Integer value1, @Param("value2") Integer value2);
+}
