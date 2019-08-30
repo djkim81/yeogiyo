9e802cb43e0a69b88ee92aec5982b2a2bdb69809
임현우 <samo57980@gmail.com>
2019-08-30 12:10:28 +0900

    ㅠㅠ..

--- yeogiyo-order_result/src/main/java/com/sk/sample/yeogiyo/orderresult/domain/repository/OrderResultRepository.java
+++ yeogiyo-order_result/src/main/java/com/sk/sample/yeogiyo/orderresult/domain/repository/OrderResultRepository.java
@@ -12,6 +12,9 @@ public interface OrderResultRepository extends PagingAndSortingRepository<Ordere
 QueryDslPredicateExecutor<Ordered> {
 	//List<Ordered> findAll(Predicate predicate);
 	Ordered findByTnum(@Param("tnum") String tnum);
+	Ordered orderAgreeByTnum(@Param("tnum") String tnum);
+	Ordered orderCancleByTnum(@Param("tnum") String tnum);
+	
 //	OrderItem findByName(@Param("name") String name);
 //	List<OrderItem> findByMenuDescriptionFoodType(@Param("foodType") FoodType foodType);
 //	List<OrderItem> findByPriceValueGreaterThanEqual(@Param("value") Integer value);
