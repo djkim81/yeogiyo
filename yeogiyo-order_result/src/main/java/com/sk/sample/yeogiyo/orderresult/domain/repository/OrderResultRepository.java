640fe9a782214bdad517d4127397604a89898fa9
임현우 <samo57980@gmail.com>
2019-08-30 10:27:56 +0900

    에러 없는 버전.

--- yeogiyo-order_result/src/main/java/com/sk/sample/yeogiyo/orderresult/domain/repository/OrderResultRepository.java
+++ yeogiyo-order_result/src/main/java/com/sk/sample/yeogiyo/orderresult/domain/repository/OrderResultRepository.java
@@ -1,18 +1,17 @@
 package com.sk.sample.yeogiyo.orderresult.domain.repository;
 
-import java.util.List;
-
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.querydsl.core.types.Predicate;
-import com.sk.sample.yeogiyo.orderresult.domain.model.OrderItem;;
+import com.sk.sample.yeogiyo.orderresult.domain.model.Ordered;
 
 @RepositoryRestResource
-public interface OrderResultRepository extends PagingAndSortingRepository<OrderItem, Long>,
-                                           QueryDslPredicateExecutor<OrderItem> {
-	List<OrderItem> findAll(Predicate predicate); 
+public interface OrderResultRepository extends PagingAndSortingRepository<Ordered, Long>,
+QueryDslPredicateExecutor<Ordered> {
+	//List<Ordered> findAll(Predicate predicate);
+	Ordered findByTnum(@Param("tnum") String tnum);
 //	OrderItem findByName(@Param("name") String name);
 //	List<OrderItem> findByMenuDescriptionFoodType(@Param("foodType") FoodType foodType);
 //	List<OrderItem> findByPriceValueGreaterThanEqual(@Param("value") Integer value);
---
+++ yeogiyo-order_result/src/main/java/com/sk/sample/yeogiyo/orderresult/domain/service/OrderResultLogic.java
@@ -0,0 +1,66 @@
+package com.sk.sample.yeogiyo.orderresult.domain.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sk.sample.yeogiyo.orderresult.domain.model.Ordered;
+import com.sk.sample.yeogiyo.orderresult.domain.repository.OrderResultRepository;
+
+@Service("orderLogic")
+public class OrderResultLogic implements OrderService {
+	@Autowired
+	private OrderResultRepository OrderResultRepository;
+
+	@Override
+	@Transactional(readOnly=true)
+	public Ordered findByTnum(String tnum) {
+		return OrderResultRepository.findByTnum(tnum);
+		//return null;
+	}
+	
+//	@Override
+//	@Transactional(readOnly=true)
+//	public Ordered findByTableNum(String table_num) {
+//		return orderRepository.findByTableNum(table_num);
+//	}
+	
+//
+	@Override
+	@Transactional
+	public Ordered update(Long id, Ordered newOrder) {
+		Ordered oldOrder = OrderResultRepository.findOne(id);
+		if(oldOrder != null) {
+			BeanUtils.copyProperties(newOrder,  oldOrder, "id");
+			return OrderResultRepository.save(oldOrder);
+		} else { 
+			return null;
+		}
+	}
+	
+//	@Override
+//	@Transactional(readOnly=true)
+//	public Ordered findByTableNum(String table_num) {
+//		return OrderResultRepository.findByTableNum(table_num);
+//	}
+	
+//
+//	@Override
+//	@Transactional
+//	public Ordered update(Long id, Ordered newOrder) {
+//		Ordered oldOrder = OrderResultRepository.findOne(id);
+//		if(oldOrder != null) {
+//			BeanUtils.copyProperties(newOrder,  oldOrder, "id");
+//			return OrderResultRepository.save(oldOrder);
+//		} else { 
+//			return null;
+//		}
+//	}
+//
+//	@Override
+//	@Transactional
+//	public void delete(Long id) {
+//		accountRepository.delete(id);
+//	}
+}
